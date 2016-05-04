3d6a1743f99150ef41b43530167ff4615a4a2844
Colin Weinshenker <caweinshenker@email.wm.edu>
2016-05-04 01:33:08 -0400

    Add cross-validation using GridSearch to KNN method

--- knn.py
+++ knn.py
@@ -11,38 +11,57 @@ Created on Mon May  2 00:01:20 2016
 
 #Results 16.09659
 
+import numpy as np
 import pandas as pd
+import sklearn.cross_validation
+import sklearn.decomposition
+import sklearn.grid_search
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.preprocessing import StandardScaler
 
 train = pd.read_csv("train_preprocessed.csv", header = 0)
 test = pd.read_csv("test_preprocessed.csv", header = 0)
-#Zero null values for the ages
-train.AgeuponOutcome_Numeric[train.AgeuponOutcome_Numeric.isnull()] = 0
-test.AgeuponOutcome_Numeric[test.AgeuponOutcome_Numeric.isnull()] = 0
 
-x_train = train.iloc[:,14:]
-y_train = train.iloc[:,12]
-x_test = test.iloc[:,10:]
-print(x_train.shape)
-print(x_test.shape)
+#Exclude date
+x_train = train.iloc[:,20:]
+y_train = train.iloc[:,17]
+x_test = test.iloc[:,11:]
 
+
+#Scale 
+sc = StandardScaler()
+sc.fit(x_train) #Calculates s and x bar
+x_train_std = sc.transform(x_train) #Performs normalization
+x_test_std = sc.transform(x_test) #Performs normalization
         
 
-#Implement a KNN model using Euclidean distance metric
-knn = KNeighborsClassifier(n_neighbors = 5, p = 2, metric = "minkowski")
+#Implement a KNN model 
+#Use grid search to cross validate to find best k
+#3 folds
+#NOTE: This can take a LONG time
+k = np.arange(4) + 1
+parameters = {'n_neighbors': k}
+knn = KNeighborsClassifier(p=2, metric = 'minkowski')
+clf = sklearn.grid_search.GridSearchCV(knn, parameters, cv = 3)
+clf.fit(x_train_std, y_train)
 
-x_train.AgeuponOutcome_Numeric.isnull()
-knn.fit(x_train, y_train)
+
 
 #Predict
-y_pred = knn.predict(x_test) #Predict the class labels for data
+y_pred = clf.predict(x_test_std)
+
+
 #Assign dummy labels based on following dictionary
-#{'Return_to_owner': 0, 'Euthanasia': 1, 'Died': 4, 'Adoption': 2, 'Transfer': 3}
-results_dict = {0: 'Return_to_owner', 1 : 'Euthanasia', 2: 'Adoption', 3: 'Transfer', 4 : 'Died'}
+#{'Return_to_owner': 0, 'Euthanasia': 1, 'Died': 4, \
+# 'Adoption': 2, 'Transfer': 3}
+results_dict = {0: 'Return_to_owner', \
+                1 : 'Euthanasia', \
+                2: 'Adoption', \
+                3: 'Transfer', 4 : 'Died'}
 rows = [0 for i in range(x_test.shape[0])]
 #'ID': [i for i in range(len(rows))], 
-data = {'Return_to_owner': rows, 'Euthanasia': rows, 'Died': rows, 'Adoption': rows, 'Transfer': rows}
+data = {'Return_to_owner': rows, 'Euthanasia': rows, \
+        'Died': rows, 'Adoption': rows, 'Transfer': rows}
 results = pd.DataFrame(data = data)
 for i in range(len(y_pred)):
     result = results_dict[y_pred[i]]
